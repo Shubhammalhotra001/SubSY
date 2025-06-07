a2705f397027a2e86ad21a56b40da715ff0258f9
Shubhammalhotra001 <shubhammalhotra9728@gmail.com>
2025-06-07 18:37:23 +0530

    Committed Final Changes In Backend

--- backend/index.js
+++ backend/index.js
@@ -2,27 +2,30 @@ const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 
+
 dotenv.config();
 const app = express();
 
 // Middleware
 app.use(express.json());
 
+// Connect MongoDB and RabbitMQ
+(async () => {
+  try {
+    await connectDB();
 
+    // Routes
+    app.use('/api/test', require('./routes/test'));
+    app.use('/api/auth', require('./routes/auth'));
+    app.use('/api/plans', require('./routes/plan'));
+    app.use('/api/subscriptions', require('./routes/subscription'));
 
-// Connect DB
-connectDB();
-
-
-// Routes
-app.use('/api/test', require('./routes/test'));
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/plans', require('./routes/plan'));
-app.use('/api/subscriptions', require('./routes/subscription'));
-
-
-
-
-app.listen(process.env.PORT, () => {
-  console.log(`🚀 Server running on port ${process.env.PORT}`);
-});
+    // Start server
+    app.listen(process.env.PORT, () => {
+      console.log(`🚀 Server running on port ${process.env.PORT}`);
+    });
+  } catch (err) {
+    console.error('❌ Error during startup:', err.message);
+    process.exit(1);
+  }
+})();
